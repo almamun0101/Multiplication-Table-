f6366cc9a87fc7563b2517e35af4c28c1a226f23
almamun0101 <almamunkhan0101@gmail.com>
2025-02-18 02:14:55 +0600

    ain

---
+++ src/script.js
@@ -0,0 +1,30 @@
+let number = document.getElementById("number1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let h1 = document.createElement("h1");
+let text = document.createTextNode("Mamun")
+h1.style.fontSize= "30px";
+h1.style.color= "";
+// h1.appendChild(text)
+// document.getElementById('output').appendChild(h1);
