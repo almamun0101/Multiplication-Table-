850b70c989e25a1ae9ca6b5126d2350aab797738
almamun0101 <almamunkhan0101@gmail.com>
2025-02-18 02:31:54 +0600

    ain

--- src/script.js
+++ src/script.js
@@ -1,30 +1,16 @@
-let number = document.getElementById("number1")
+let numberOne = document.getElementById("1")
+let output = document.getElementById("output")
+let button = document.getElementById("submit")
+const li = document.createElement("li")
+let result;
 
+button.addEventListener('click',function (){
 
+  for (let i = 1; i <=10; i++) {
+    // const element = array[i];
+    output.innerHTML=i;
+    console.log(i) 
+    
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let h1 = document.createElement("h1");
-let text = document.createTextNode("Mamun")
-h1.style.fontSize= "30px";
-h1.style.color= "";
-// h1.appendChild(text)
-// document.getElementById('output').appendChild(h1);
+})
